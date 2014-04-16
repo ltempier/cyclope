52d541f8507113036ec24787d57e2d2eaf137278
ltempier <laurent.tempier@gmail.com>
2014-04-17 00:04:21 +0200

    display the json response in the terminal

--- cyclope.ino
+++ cyclope.ino
@@ -1,11 +1,37 @@
 #include <Ethernet.h>
 
-byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress server(173,194,33,104);
-
+byte mac[] = {  
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress server(213,41,121,149);
 
 EthernetClient client;
 
-  
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+  }
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+  }
+  delay(1000);
+  Serial.println("connecting...");
+  if (client.connect(server, 80)) {
+    Serial.println("connected");
+    client.println("GET /vls/v1/stations?contract=paris&apiKey=833f7a451533ba92a0.................");
+    client.println();
+  } 
+  else {
+    Serial.println("connection failed");
+  }
+}
+void loop(){
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+}
+
+
+
 
 
