ba0e2b8301be2c11657b3f0a3bf2d025b38d6739
Baptiste Adrien <adrien.baptiste@gmail.com>
2014-04-19 10:55:24 +0200

    Switch ip to api domain name, fix import

--- cyclope.ino
+++ cyclope.ino
@@ -1,8 +1,9 @@
+#include <SPI.h>
 #include <Ethernet.h>
 
 byte mac[] = {  
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress server(213,41,121,149);
+char server[] = "https://api.jcdecaux.com";  
 
 EthernetClient client;
 
@@ -17,7 +18,7 @@ void setup() {
   Serial.println("connecting...");
   if (client.connect(server, 80)) {
     Serial.println("connected");
-    client.println("GET /vls/v1/stations?contract=paris&apiKey=833f7a451533ba92a0.................");
+    client.println("GET /vls/v1/stations?contract=paris&apiKey=833f7a451533ba92a04cf8fcbb840fff9612cae8");
     client.println();
   } 
   else {
@@ -28,6 +29,17 @@ void loop(){
   if (client.available()) {
     char c = client.read();
     Serial.print(c);
+  }
+  
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // do nothing forevermore:
+    for(;;)
+      ;
   }
 }
 
